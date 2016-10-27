31f1c75ae6a285906f6e113512bc7984a2b1ee88
Michael <michaelcodinglife@icloud.com>
2016-10-27 22:08:07 +0800

    Completed automatic sprite generation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,10 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
 svgSrpite = require('gulp-svg-sprite'),
-rename = require('gulp-rename');
+rename = require('gulp-rename'),
+del = require('del');
 
 var config = {
 	mode: {
 		css: {
+			sprite: 'sprite.svg',
 			render: {
 				css: {
 					template: './gulp/templates/sprite.css'
@@ -14,14 +16,38 @@ var config = {
 	}
 }
 
-gulp.task('createSprite', function() {
+gulp.task('beginClean', function() {
+	return del(['./tem/sprite', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function() {
 	return gulp.src('./app/assets/images/icons/**/*.svg')
 		.pipe(svgSrpite(config))
 		.pipe(gulp.dest('./app/tem/sprite/'));
 });
 
-gulp.task('copySpriteCSS', function() {
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+	return gulp.src('./app/tem/sprite/css/**/*.svg')
+		.pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS', ['createSprite'], function() {
 	return gulp.src('./app/tem/sprite/css/*.css')
 		.pipe(rename('_sprite.css'))
 		.pipe(gulp.dest('./app/assets/styles/modules'));
 });
+
+gulp.task('endClean', ['copySpriteGraphic','copySpriteCSS'], function() {
+	return del('./app/tem/sprite');
+});
+
+gulp.task('icons', ['beginClean','createSprite','copySpriteGraphic','copySpriteCSS','endClean']);
+
+
+
+
+
+
+
+
+
